8fa5062c90ba9b9a63323f45ef2fca048f6be145
Scott Nealon <nealon.scott@gmail.com>
2023-10-07 19:34:16 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="lolalytics scraper",
+    package_dir={"": "lolalytics_scraper"},
+    license="MIT",
+    version="0.0",
+    author="Scott Nealon",
+    author_email="nealon.scott@gmail.com",
+)
