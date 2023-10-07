45ee1aaba2f6a6522626d7767f0446313f1b7ec3
Scott Nealon <nealon.scott@gmail.com>
2023-10-07 19:49:53 -0400

    More setup.py changes

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 with open(Path(__file__).parent.joinpath("requirements.txt"), "r") as file_handle:
     requirements = file_handle.readlines()
 
 setup(
     name="lolalytics scraper",
-    package_dir={"": "lolalytics_scraper"},
+    package_dir=find_packages(),
     license="MIT",
     version="0.0",
     author="Scott Nealon",
