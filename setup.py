b119bb72d6f3cedda8e60707cc0f7a82355eac6e
Scott Nealon <nealon.scott@gmail.com>
2023-10-17 17:26:17 -0400

    What if I do it this way?

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-from pathlib import Path
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-with open(Path(__file__).parent.joinpath("requirements.txt"), "r") as file_handle:
-    requirements = file_handle.readlines()
-
-setup(
-    name="lolalytics_scraper",
-    package_dir=find_packages(),
-    license="MIT",
-    version="0.0",
-    author="Scott Nealon",
-    author_email="nealon.scott@gmail.com",
-    install_requires=requirements,
-    package_data={"lolalytics_scraper": ["data/*"]},
-)
+setup()
